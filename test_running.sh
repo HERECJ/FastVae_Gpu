2322b77a1d5118ec3185eb3fe6cc1afb203f9a5a
chenjin <2014060201023@std.uestc.edu.cn>
2021-09-18 19:09:37 +0800

    sh file

--- test_running.sh
+++ test_running.sh
@@ -16,9 +16,9 @@ do
         # for s in 0 1 2 3 4
         for s in 4
         do
-            python run_multi_items.py -data 'amazoni' -e 50 --num_workers 8 -lr ${lr} -b $b --log_path ${logs} --sampler $s --weight_decay $w -s 2000
+            python run_mm.py -data 'amazoni' -e 50 --num_workers 8 -lr ${lr} -b $b --log_path ${logs} --sampler $s --weight_decay $w -s 2000
 
-            python run_multi_items.py -data 'amazoni' -e 50 --num_workers 8 -lr ${lr} -b $b --log_path ${logs} --sampler $s --weight_decay $w -s 20
+            python run_mm.py -data 'amazoni' -e 50 --num_workers 8 -lr ${lr} -b $b --log_path ${logs} --sampler $s --weight_decay $w -s 20
 
         done
     done
